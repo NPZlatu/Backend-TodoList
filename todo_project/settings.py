be3bc6e558bdc196d52872fea75910a7a4573d13
Niraj Paudel <itsmenirajpaudel@gmail.com>
2025-03-20 11:05:17 +1300

    Add security from dotenv

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-crmiyybl!o=90!1vh9dh@22d*bu%0sjb^+t8v@s)^pel@jn3c#'
+SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-if-env-not-set')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
