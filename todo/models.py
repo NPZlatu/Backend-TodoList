8bf3a22022017a63d81727fc34c48b3b3ece1424
Niraj Paudel <itsmenirajpaudel@gmail.com>
2025-03-29 11:57:54 +1300

    Make description optional

--- todo/models.py
+++ todo/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Todo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True) 
     completed = models.BooleanField(default=False)
 
     def __str__(self):
