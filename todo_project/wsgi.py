009d237a13d3207e300c32583d813f851c37e60f
Niraj Paudel <itsmenirajpaudel@gmail.com>
2025-03-30 16:01:15 +1300

    Add WSGI

--- todo_project/wsgi.py
+++ todo_project/wsgi.py
@@ -1,15 +1,11 @@
-"""
-WSGI config for todo_project project.
-Modified for Vercel deployment.
-"""
-
 import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_project.settings')
 
-# Django's standard WSGI application test
+# Standard Django WSGI app
 application = get_wsgi_application()
 
-# Vercel requires this variable
-handler = application  # Same as `app` if using Flask/FastAPI
+# Vercel requires this exact variable name
+app = application  # Some Vercel setups need 'app'
+handler = application  # Others need 'handler'
