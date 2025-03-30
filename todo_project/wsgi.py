54ea53896a1d93ef9af69a79658df1e95356bce6
Niraj Paudel <itsmenirajpaudel@gmail.com>
2025-03-30 19:24:13 +1300

    Deployed in vercel

--- todo_project/wsgi.py
+++ todo_project/wsgi.py
@@ -1,26 +1,16 @@
+"""
+WSGI config for api project.
+
+It exposes the WSGI callable as a module-level variable named ``app``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
 import os
+
 from django.core.wsgi import get_wsgi_application
-from django.core.handlers.wsgi import WSGIRequest
-from django.http import HttpResponse
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_project.settings')
 
-application = get_wsgi_application()
-
-def handler(request):
-    """Handle incoming requests to the Django application."""
-    # Create a WSGI environment for the request
-    environ = request.environ
-    environ['wsgi.input'] = request.body
-    environ['REQUEST_METHOD'] = request.method
-    environ['PATH_INFO'] = request.path
-    environ['QUERY_STRING'] = request.query_string
-
-    # Ensure the request is passed to Django's application
-    response = application(environ, start_response)
-
-    return HttpResponse(response)
-
-def start_response(status, response_headers, exc_info=None):
-    """A simple WSGI start_response function."""
-    return None
+app = get_wsgi_application()
