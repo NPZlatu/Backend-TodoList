03a4f41169fc77f0e7c7c7a6d8e9fabd75f0da13
Niraj Paudel <itsmenirajpaudel@gmail.com>
2025-03-30 15:58:16 +1300

    Fix WSGI

--- todo_project/wsgi.py
+++ todo_project/wsgi.py
@@ -8,7 +8,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_project.settings')
 
-# Django's standard WSGI application
+# Django's standard WSGI application test
 application = get_wsgi_application()
 
 # Vercel requires this variable
