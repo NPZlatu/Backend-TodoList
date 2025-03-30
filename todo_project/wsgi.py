7a4cb3563dc13a676e8307a8feed64fefe4db577
Niraj Paudel <itsmenirajpaudel@gmail.com>
2025-03-30 15:55:59 +1300

    Fix WSGI

--- todo_project/wsgi.py
+++ todo_project/wsgi.py
@@ -1,16 +1,15 @@
 """
 WSGI config for todo_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+Modified for Vercel deployment.
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_project.settings')
 
+# Django's standard WSGI application
 application = get_wsgi_application()
+
+# Vercel requires this variable
+handler = application  # Same as `app` if using Flask/FastAPI
